ed5ddf68059f319ec87ad44e7a7d14cb5db37981
Jasper Van der Jeugt <m@jaspervdj.be>
2012-10-28 14:19:29 +0100

    Print more info

--- src/WhatMorphism/Pass.hs
+++ src/WhatMorphism/Pass.hs
@@ -1,25 +1,23 @@
 --------------------------------------------------------------------------------
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 module WhatMorphism.Pass
     ( whatMorphismPass
     ) where
 
 
 --------------------------------------------------------------------------------
-import           GhcPlugins
+import           Control.Monad (liftM2)
+import           CoreMonad
+import           CoreSyn
+import           Outputable
 
 
 --------------------------------------------------------------------------------
 whatMorphismPass :: [CoreBind] -> CoreM [CoreBind]
 whatMorphismPass binds = do
-    tracePretty binds
+    mapM_ whatMorphism binds
     return binds
-
-
---------------------------------------------------------------------------------
-tracePretty :: Outputable a => a -> CoreM ()
-tracePretty x = do
-    dflags <- getDynFlags
-    putMsgS $ "WhatMorphism: " ++ showSDoc dflags (ppr x)
 
 
 --------------------------------------------------------------------------------
@@ -31,3 +29,70 @@ tracePretty x = do
 --
 -- 3. In this destruction we get a number of binds. Recursive calls can only be
 -- made on these binds.
+
+
+--------------------------------------------------------------------------------
+whatMorphism :: CoreBind -> CoreM ()
+whatMorphism (NonRec _ _) = return ()
+whatMorphism (Rec recs)   = mapM_ (uncurry whatMorphismRec) recs
+
+
+--------------------------------------------------------------------------------
+whatMorphismRec :: CoreBndr -> Expr CoreBndr -> CoreM ()
+whatMorphismRec name expr = do
+    message $ "Analyzing " .++. pretty name
+    message $ "Args: " .++. pretty args
+    message $ "Body: " .++. pretty body
+    return ()
+  where
+    (args, body) = arguments expr
+
+
+--------------------------------------------------------------------------------
+-- | Collects the arguments to a function
+arguments :: Expr CoreBndr -> ([CoreBndr], Expr CoreBndr)
+arguments = go []
+  where
+    go xs (Lam x e) = go (x : xs) e
+    go xs e         = (reverse xs, e)
+
+
+--------------------------------------------------------------------------------
+data Trace = Trace {unTrace :: CoreM String}
+
+
+--------------------------------------------------------------------------------
+class ToTrace t where
+    toTrace :: t -> Trace
+
+
+--------------------------------------------------------------------------------
+instance ToTrace Trace where
+    toTrace = id
+
+
+--------------------------------------------------------------------------------
+instance ToTrace String where
+    toTrace = Trace . return
+
+
+--------------------------------------------------------------------------------
+instance ToTrace (CoreM String) where
+    toTrace = Trace
+
+
+--------------------------------------------------------------------------------
+message :: ToTrace a => a -> CoreM ()
+message x = unTrace (toTrace x) >>= putMsgS
+
+
+--------------------------------------------------------------------------------
+(.++.) :: (ToTrace a, ToTrace b) => a -> b -> Trace
+x .++. y = Trace $ liftM2 (++) (unTrace $ toTrace x) (unTrace $ toTrace y)
+
+
+--------------------------------------------------------------------------------
+pretty :: Outputable a => a -> CoreM String
+pretty x = do
+    dflags <- getDynFlags
+    return $ showSDoc dflags $ ppr x
