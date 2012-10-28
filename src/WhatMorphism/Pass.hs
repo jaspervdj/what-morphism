57444cfcdcb89242229dd51e2aab132d70888441
Jasper Van der Jeugt <m@jaspervdj.be>
2012-10-28 12:23:12 +0100

    Type some stuff

--- src/WhatMorphism/Pass.hs
+++ src/WhatMorphism/Pass.hs
@@ -20,3 +20,14 @@ tracePretty :: Outputable a => a -> CoreM ()
 tracePretty x = do
     dflags <- getDynFlags
     putMsgS $ "WhatMorphism: " ++ showSDoc dflags (ppr x)
+
+
+--------------------------------------------------------------------------------
+-- 1. Descend into the bind, which should be a lambda. We need to figure out it
+-- arguments, one of these might be 'destructed'.
+--
+-- 2. If in the body of the lambda we find a case statement which destructs any
+-- of these arguments, we have a good hint.
+--
+-- 3. In this destruction we get a number of binds. Recursive calls can only be
+-- made on these binds.
