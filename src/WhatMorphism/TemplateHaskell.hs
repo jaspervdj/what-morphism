759c4387ccad15f0e9bfaf1c8aaf463b73da1b23
Jasper Van der Jeugt <m@jaspervdj.be>
2012-12-04 11:38:42 +0100

    Generate non-recursive folds

--- src/WhatMorphism/TemplateHaskell.hs
+++ src/WhatMorphism/TemplateHaskell.hs
@@ -5,6 +5,7 @@ module WhatMorphism.TemplateHaskell
 
 
 --------------------------------------------------------------------------------
+import           Control.Monad       (forM, replicateM)
 import           Language.Haskell.TH
 
 
@@ -22,16 +23,32 @@ deriveFold typeName = do
 --------------------------------------------------------------------------------
 mkFold :: Name -> [TyVarBndr] -> [Con] -> Q [Dec]
 mkFold typeName typeBndrs cons = do
-    a <- newName "a"  -- Result type of the fold
+    a      <- newName "a"  -- Result type of the fold
+    consFs <- forM cons $ \c -> do
+        f  <- newName "f"
+        ys <- replicateM (length $ conTypes c) (newName "y")
+        return $ (c, f, ys)
+    x      <- newName "x"  -- Argument we're destroying
 
     return
         [ SigD foldName $ ForallT (typeBndrs ++ [PlainTV a]) [] $
             mkFunTy
-                [mkAppTy typeName typeBndrs]
+                ([mkFunTy (conTypes con) (VarT a) | con <- cons] ++
+                    [mkAppTy typeName typeBndrs])
                 (VarT a)
 
         , FunD foldName
-            [ Clause [] (NormalB (VarE $ mkName "undefined")) []
+            [ Clause
+                ([VarP f | (_, f, _) <- consFs] ++ [VarP x])
+                (NormalB
+                    (CaseE (VarE x)
+                        [ Match
+                            (ConP (conName c) (map VarP ys))
+                            (NormalB $ mkAppE (VarE f) ys)
+                            []
+                        | (c, f, ys) <- consFs
+                        ]))
+                []
             ]
         ]
   where
@@ -46,6 +63,12 @@ mkAppTy name (KindedTV x _ : xs) = AppT (mkAppTy name xs) (VarT x)
 
 
 --------------------------------------------------------------------------------
+mkAppE :: Exp -> [Name] -> Exp
+mkAppE f []       = f
+mkAppE f (a : as) = mkAppE (AppE f (VarE a)) as
+
+
+--------------------------------------------------------------------------------
 -- mkFunTy :: Type -> Type -> Type
 -- mkFunTy x y = (AppT (AppT ArrowT x) y)
 
@@ -57,10 +80,20 @@ mkFunTy [] y       = y
 
 
 --------------------------------------------------------------------------------
-conTypes :: Con -> Q [Type]
-conTypes (NormalC _ ts)   = return [t | (_, t) <- ts]
-conTypes (RecC _ ts)      = return [t | (_, _, t) <- ts]
-conTypes (InfixC t1 _ t2) = return $ map snd [t1, t2]
-conTypes (ForallC _ _ _)  = fail $
+conName :: Con -> Name
+conName (NormalC n _)   = n
+conName (RecC n _)      = n
+conName (InfixC _ n _)  = n
+conName (ForallC _ _ _) = error $
+    "WhatMorphism.TemplateHaskell.conTypes: " ++
+    "cannot yet define folds for forall'd types"
+
+
+--------------------------------------------------------------------------------
+conTypes :: Con -> [Type]
+conTypes (NormalC _ ts)   = [t | (_, t) <- ts]
+conTypes (RecC _ ts)      = [t | (_, _, t) <- ts]
+conTypes (InfixC t1 _ t2) = map snd [t1, t2]
+conTypes (ForallC _ _ _)  = error $
     "WhatMorphism.TemplateHaskell.conTypes: " ++
     "cannot yet define folds for forall'd types"
